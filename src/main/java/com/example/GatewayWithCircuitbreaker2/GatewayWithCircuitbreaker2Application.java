f0a94bc44ef162877003468f5e94fb68667f4d86
Eugene_Mamychkin <eugenemamychkin@gmail.com>
2025-07-20 23:15:08 +0300

    init commit

---
+++ src/main/java/com/example/GatewayWithCircuitbreaker2/GatewayWithCircuitbreaker2Application.java
@@ -0,0 +1,22 @@
+package com.example.GatewayWithCircuitbreaker2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+///**
+// * Gateway + Circuitbreaker
+// *
+// * Все настройки произведены через aplication.yml
+// * В конфигурации находятся только настройки для регистрации в Eureka и поиска нужного конфига на ConfigServer
+// * Сам конфиг gateway-dev.yml лежит в репозитории гита
+// * Реализован поиск ConfigServer по регистру Eureka
+// *
+// */
+//@SpringBootApplication
+//public class GatewayWithCircuitbreaker2Application {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(GatewayWithCircuitbreaker2Application.class, args);
+//	}
+//
+//}
---
+++ src/test/java/com/example/GatewayWithCircuitbreaker2/GatewayWithCircuitbreaker2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GatewayWithCircuitbreaker2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayWithCircuitbreaker2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
